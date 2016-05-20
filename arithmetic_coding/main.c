83be9b0ca503e172ddd2f35974fd9bdc41b62632
Wang Louis <wang.hao@megachips.co.jp>
2016-05-20 23:21:49 +0900

    Initial Commit

---
+++ arithmetic_coding/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  arithmetic_coding
+//
+//  Created by WangHao on 5/20/16.
+//  Copyright © 2016 hsw. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
